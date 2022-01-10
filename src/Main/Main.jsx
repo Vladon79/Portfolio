1ea10ef8337fe7a8add8a8072dd1e377346eaaaa
Vladon79 <vla3ik@gmail.com>
2022-01-10 16:12:57 +0300

    new_commit

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
       <div className={styleContainer.container}>
         <div className={style.text}>
           <span>Привет</span>
-          <h1>Меня зовут Клопот Влод</h1>
+          <h1>Меня зовут Клопот Влад</h1>
           <p>Я front-end разработчик</p>
         </div>
         <div className={style.ava}></div>
