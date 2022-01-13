7252b6f6de651c2019cb12e313f5228e4535d4de
Vladon79 <vla3ik@gmail.com>
2022-01-13 14:09:52 +0300

    scss3_commit

---
+++ src/Common/components/title/Button.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import style from './Button.module.scss';
+
+const Button = (props) => {
+    return (
+        <div>
+            <button type='submit' className={style.button}>{props.title}</button>
+      </div>
+    )
+}
+
+export default Button;
--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,21 +1,21 @@
 import react from 'react';
-import style from './Contact.module.css';
+import style from './Contact.module.scss';
 import styleContainer from './../Common/Stiles/Conteiner.module.css';
+import Title from '../Common/components/title/Title';
+import Button from '../Common/components/title/Button';
 
 const Contact = (props) => {
   return (
     <div className={style.contact}>
-      <div className={`${styleContainer.container} ${style.contactContsiner}`}>
-      <div className={style.blockContact}>
-      <h2 className={style.title}>Контакты</h2>
+      <div className={`${styleContainer.container} ${style.contactContainer}`}>
+      <Title title={'Contact'}/>
       <form className={style.inputs}>
-        <input className={style.input}/>
-        <input className={style.input}/>
-        <textarea className={style.textarea}/>
-        <button type='submit'>Отправить</button>
+        <input className={style.input} placeholder={'Name*'}/>
+        <input className={style.input} placeholder={'Email*'}/>
+        <textarea className={style.textarea} placeholder={'Message*'}/>
+        <Button title={'Submit'}/>
         </form>
         </div>
-      </div>
     </div>
   );
 }
