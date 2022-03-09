03310473ec8f8ea719e448c77048853bb0df23d9
Vladon79 <vla3ik@gmail.com>
2022-03-09 11:07:07 +0300

    new_color

--- src/App.js
+++ src/App.js
@@ -5,20 +5,32 @@ import Header from './Header/Header';
 import Main from './Main/Main';
 import MyProject from './MyProject/MyProject';
 import RemoteWork from './RemoteWork/RemoteWork';
-import Skils from './Skills/Skills';
+import Skills from './Skills/Skills';
+import {Route, Router, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Skils/>
-      <MyProject/>
-      <Contact/>
-      <RemoteWork/>
-      <Footer/>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*<Router />*/}
+            <Header/>
+
+                {/*<Routes>*/}
+                {/*    <Route path="/"/>*/}
+                {/*    <Route path="/home" element={<Main/>}/>*/}
+                {/*    <Route path="/skills" element={<Skills/>}/>*/}
+                {/*    <Route path="/myProject" element={<MyProject/>}/>*/}
+                {/*    <Route path="/contact" element={<Contact/>}/>*/}
+                {/*    /!*<Route path="/test" element={<TestPage/>}/>s*!/*/}
+                {/*</Routes>*/}
+            <Main/>
+            <Skills/>
+            <MyProject/>
+            <Contact/>
+            {/*<RemoteWork/>*/}
+            <Footer/>
+
+        </div>
+    );
 }
 
 export default App;
--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,17 +1,22 @@
-import react from 'react';
 import style from './Nav.module.scss';
+import {NavLink} from "react-router-dom";
 
 const Nav = () => {
-  return (
-    <div className={style.nav}>
+    return (
+        <div className={style.nav}>
+            <NavLink children={'Home'} to={'/home'} className={({isActive}) => (isActive ? style.linkActive : style.link)}/>
+            <NavLink children={'Skills'}to={'/skills'} className={({isActive}) => (isActive ? style.linkActive : style.link)}/>
+            <NavLink children={'My project'} to={'/myProject'}className={({isActive}) => (isActive ? style.linkActive : style.link)}/>
+            <NavLink children={'Contact'}to={'/contact'} className={({isActive}) => (isActive ? style.linkActive : style.link)}/>
 
-       <a href="">Home</a>
-       <a href="">Skills</a>
-       <a href="">My project</a>
-       <a href="">Contact</a>
-       
-    </div>
-  );
+
+            {/*<a href="">Home</a>*/}
+            {/*<a href="">Skills</a>*/}
+            {/*<a href="">My project</a>*/}
+            {/*<a href="">Contact</a>*/}
+
+        </div>
+    );
 }
 
 export default Nav;
