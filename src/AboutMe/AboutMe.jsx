e37f9b92ba055396c66bbd9cfd0eb4f3385987c7
Vladon79 <vla3ik@gmail.com>
2022-04-11 15:36:54 +0300

    about_me2

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -2,6 +2,7 @@ import style from './AboutMe.module.scss';
 import styleContainer from './../Common/Stiles/Conteiner.module.css';
 import Title from '../Common/components/title/Title';
 import Fade from 'react-reveal/Fade';
+import Tilt from 'react-tilt'
 
 
 const AboutMe = () => {
@@ -12,13 +13,23 @@ const AboutMe = () => {
                 <Fade bottom>
                     <Title title={'About me'}/>
                     <div className={style.infoForMe}>
-                        <p> I'm frontend developer with experience in creating SPA using React, React Native,
-                            TypeScript,
-                            JavaScript, SCSS, HTML. I'm planning to study Node.js, because in the future i see myself as
-                            a
-                            Full-stack developer. Usually i prefer to spend my leisure time on improving my English,
-                            studying new technologies and solve tasks on Codewars. Ready to consider project work and
-                            full-time employment.</p>
+
+                        <div className={style.imageContainer}>
+                            <section className={style.border}>
+                                <Tilt className={style.tilt} options={{scale: 1, max: 25, perspective: 2000,}}>
+                                    <div className={style.image}></div>
+                                </Tilt>
+                            </section>
+                        </div>
+
+                        <div className={style.info}>
+                            <p> I'm frontend developer with experience in creating SPA with React/Redux/ TypeScript in
+                                team. Now I improve my skills in this direction and expanding them with new
+                                technologies. I spend my leisure time on Codewars or by improving english skills. Open
+                                to your suggestions.</p>
+
+                        </div>
+
                     </div>
                 </Fade>
             </div>
