a6521b6d56b32844878fbb02ca170a7ee1773f63
Vladon79 <vla3ik@gmail.com>
2022-04-13 13:22:14 +0300

    update_logo

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -45,12 +45,11 @@ const Contact = (props) => {
                             </div>
                         )}
                     </div>
-
                     <form className={style.inputs}>
                         <input className={style.input} placeholder={'Name*'}/>
                         <input className={style.input} placeholder={'Email*'}/>
                         <textarea className={style.textarea} placeholder={'Message*'}/>
-                        <Button title={'Submit'}/>
+                        <Button title={'Send message'}/>
                     </form>
                 </Fade>
             </div>
